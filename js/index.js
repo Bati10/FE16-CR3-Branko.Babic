126e2522274f6bc01222b873b353c18ebb83d6a0
Bati10 <branko.babic10@gmail.com>
2022-05-28 09:14:07 +0200

    first code

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// first task
+var pinkFloyd = 150;
+var ramones = 80;
+var ledZeppelin = 120;
+var velvetUnderground = 250;
+var theGratefulDead = 50;
+var u2 = 340;
+var theRollingStones = 210;
+var theBeatles = 120;
+var bobMarley = 220;
+
+// second task 
+let fest_day1 = (pinkFloyd + theBeatles + theGratefulDead);
+let fest_day2 = (velvetUnderground + ramones + theRollingStones);
+let fest_day3 = (ledZeppelin + u2 + bobMarley);
+
+// third task 
+let total = fest_day1 + fest_day2 + fest_day3
+
+console.log(`On the first day of the festival, I was lucky to watch bands 1,2,3.`);
+console.log(`Then I watched the bands 1,2 and 3 on the second day and it cost me $${fest_day2} in total that day.`);
+console.log(`I was a bit tired on the third day but it was so much worth it to watch the bands 1, 2, 3 and spent $${total} for the three days od the festival.`)
+
+
+// fourth task 
+var saved = u2 * 0.3;
+var discount = u2 - saved;
+
+console.log(`If I had booked the concert with enough time in advence, it would have cost me a total of $${discount} and I would have saved $${saved}.`)
